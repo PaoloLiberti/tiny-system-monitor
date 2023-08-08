8110e0733b4bb77dadf2d4f24c5c05af3f0f8967
Paolo Liberti <paololiberti@MacBook-Pro-3.local>
2023-08-08 15:32:53 +0200

    commit

--- src/host/run.py
+++ src/host/run.py
@@ -4,6 +4,7 @@ import gpu
 import cpu
 import struct
 
+
 def get_cpu_temperature():
     temperature = psutil.sensors_temperatures()
     if 'cpu-thermal' in temperature:
@@ -14,7 +15,7 @@ def get_cpu_temperature():
 
 
 ser = serial.Serial(
-    '/dev/tty.usbmodem14314101',  # Change this lino to the port the device is connected
+    '/dev/tty.usbmodem14312101',  # Change this lino to the port the device is connected
     baudrate=115200,
     timeout=0.2)
 
@@ -37,8 +38,10 @@ while True:
         disk_percent = psutil.disk_usage('/').percent
 
     net_stat = psutil.net_io_counters()
-    bits_sent_per_sec = (psutil.net_io_counters().bytes_sent - acc_bytes_sent) * 8
-    bits_recv_per_sec = (psutil.net_io_counters().bytes_recv - acc_bytes_recv) * 8
+    bits_sent_per_sec = (
+        psutil.net_io_counters().bytes_sent - acc_bytes_sent) * 8
+    bits_recv_per_sec = (
+        psutil.net_io_counters().bytes_recv - acc_bytes_recv) * 8
     if bits_recv_per_sec < 0:
         bits_recv_per_sec = 0
     if bits_sent_per_sec < 0:
@@ -50,7 +53,7 @@ while True:
 
     message = struct.pack("bbbbIIf", cpu_percent, round(gpu_usage.usage), int(psutil.virtual_memory().percent),
                           int(disk_percent), int(bits_recv_per_sec), int(bits_sent_per_sec), round(temp_sensor.temperature))
-    
+
     # print("🚀 ~ file: run.py:48 ~ message:", message)
 
     ser.write(message)
